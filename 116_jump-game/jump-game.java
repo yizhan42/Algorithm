c7250a17febbd03a8e9ffe08e0cb8e176de6bba8
Yi Zhan <yizhan42@gmail.com>
2018-11-13 11:39:19 +0800

    Add files via upload

---
+++ 116_jump-game/jump-game.java
@@ -0,0 +1,33 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/jump-game
+@Language: Java
+@Datetime: 17-02-25 15:58
+*/
+
+// version 1: Dynamic Programming
+// 这个方法，复杂度是 O(n^2) 可能会超时，但是依然需要掌握。
+
+public class Solution {
+    /**
+     * @param A: A list of integers
+     * @return: The boolean answer
+     */
+    public boolean canJump(int[] A) {
+        // wirte your code here
+        boolean[] can = new boolean[A.length];
+        can[0] = true;
+        
+        for (int i = 1; i < A.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (can[j] && j + A[j] >= i) {
+                    can[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return can[A.length - 1];
+    }
+}
