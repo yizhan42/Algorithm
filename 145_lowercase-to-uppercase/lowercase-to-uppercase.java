c96a5e50d132cb532177b6dbce6b8c4f9be1abe4
Yi Zhan <yizhan42@gmail.com>
2018-11-13 20:21:53 +0800

    Add files via upload

---
+++ 145_lowercase-to-uppercase/lowercase-to-uppercase.java
@@ -0,0 +1,25 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/lowercase-to-uppercase
+@Language: Java
+@Datetime: 16-03-29 01:56
+*/
+
+public class Solution {
+    /**
+     * @param character a character
+     * @return a character
+     */
+    public char lowercaseToUppercase(char character) {
+        // Write your code here
+        
+        if( character >= 'a' && character <='z'){
+          
+           return (char)(character - 32) ;
+        }
+        else {
+            return character;
+        }
+    }
+}
