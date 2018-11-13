e860a5645ffbaecea20660a74c8894c4ea09ee31
Yi Zhan <yizhan42@gmail.com>
2018-11-13 20:17:29 +0800

    Add files via upload

---
+++ 128_hash-function/hash-function.java
@@ -0,0 +1,23 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/hash-function
+@Language: Java
+@Datetime: 17-02-25 15:33
+*/
+
+class Solution {
+    /**
+     * @param key: A String you should hash
+     * @param HASH_SIZE: An integer
+     * @return an integer
+     */
+    public int hashCode(char[] key,int HASH_SIZE) {
+        // write your code here
+        long ans = 0;
+        for(int i = 0; i < key.length;i++) {
+            ans = (ans * 33 + (int)(key[i])) % HASH_SIZE; 
+        }
+	return (int)ans;
+    }
+};
