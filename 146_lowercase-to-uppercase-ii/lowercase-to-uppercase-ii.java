37168df19468693069cbf65a30365d40423e1c7e
Yi Zhan <yizhan42@gmail.com>
2018-11-13 20:22:19 +0800

    Add files via upload

---
+++ 146_lowercase-to-uppercase-ii/lowercase-to-uppercase-ii.java
@@ -0,0 +1,27 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/lowercase-to-uppercase-ii
+@Language: Java
+@Datetime: 16-03-29 04:19
+*/
+
+public class Solution {
+    /**
+     * @param str a string
+     * @return a string
+     */
+ 
+        public String lowercaseToUppercase2(String str) {
+        // Write your code here
+        StringBuilder sb = new StringBuilder(str);
+        for (int index = 0; index < sb.length(); index++) {
+            char c = sb.charAt(index);
+            if (Character.isLowerCase(c)) {
+                sb.setCharAt(index, Character.toUpperCase(c));
+            }
+        }
+        return sb.toString();
+    }
+    
+}
