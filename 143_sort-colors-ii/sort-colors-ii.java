d32334b11e0e07480604feaa8c5787804bb0283e
Yi Zhan <yizhan42@gmail.com>
2018-11-13 20:21:30 +0800

    Add files via upload

---
+++ 143_sort-colors-ii/sort-colors-ii.java
@@ -0,0 +1,55 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/sort-colors-ii
+@Language: Java
+@Datetime: 16-11-13 14:04
+*/
+
+class Solution {
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2(int[] colors, int k) {
+        // write your code here
+        int count = 0;
+        int start = 0;
+        int end = colors.length-1;
+        while (count < k) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            
+            for (int i = start; i <= end; i++) {
+                min = Math.min(min, colors[i]);
+                max = Math.max(max, colors[i]);
+            }
+            int left = start;
+            int right = end;
+            int cur = left;
+            while(cur <= right) {
+                if (colors[cur] == min) {
+                    swap(left, cur, colors);
+                    cur++;
+                    left++;
+                } else if (colors[cur] > min && colors[cur] < max) {
+                    cur++;
+                } else {
+                    int tmp = colors[cur];
+                    swap(cur, right, colors);
+                    right--;
+                }
+            }
+            count += 2;
+            start = left;
+            end = right;
+        }
+    
+    }
+    void swap(int left, int right, int[] colors) {
+        int tmp = colors[left];
+        colors[left] = colors[right];
+        colors[right] = tmp;
+    }
+}
