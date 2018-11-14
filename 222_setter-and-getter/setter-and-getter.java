fef7a9374d7ffea18048ab1044c6434d54167d24
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:07:05 +0800

    Add files via upload

---
+++ 222_setter-and-getter/setter-and-getter.java
@@ -0,0 +1,34 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/setter-and-getter
+@Language: Java
+@Datetime: 16-03-20 03:29
+*/
+
+public class School {
+    /*
+     * Declare a private attribute *name* of type string.
+     */
+    
+    // write your code here
+      private String name;
+      
+    /**
+     * Declare a setter method `setName` which expect a parameter *name*.
+     */
+    // write your code here
+    public void setName(String name){
+        this.name = name;
+    }
+    /**
+     * Declare a getter method `getName` which expect no parameter and return
+     * the name of this school
+     */
+    // write your code here
+    public String getName(){
+       
+        
+        return this.name;
+    }
+}
