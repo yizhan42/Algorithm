4f3a17bff75c373b0c8da055964c2e94959a24e2
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:05:12 +0800

    Add files via upload

---
+++ 214_max-of-array/max-of-array.java
@@ -0,0 +1,26 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/max-of-array
+@Language: Java
+@Datetime: 16-03-09 05:04
+*/
+
+public class Solution {
+    /**
+     * @param A a float array
+     * @return a float number
+     */
+    public float maxOfArray(float[] A) {
+        // Write your code here
+        int i;
+      
+        float max = A[0];
+        for(i = 1; i < A.length ;i++){
+            if(A[i] > max){
+                max = A[i];
+            }
+        }
+        return max;
+    }
+}
