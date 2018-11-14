01ed6b8d8ff1835c6931f61ee075348af31a2cf5
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:00:22 +0800

    Add files via upload

---
+++ 173_insertion-sort-list/insertion-sort-list.java
@@ -0,0 +1,45 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/insertion-sort-list
+@Language: Java
+@Datetime: 16-11-10 08:39
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        // write your code here
+         ListNode dummy = new ListNode(0);
+        // 这个dummy的作用是，把head开头的链表一个个的插入到dummy开头的链表里
+        // 所以这里不需要dummy.next = head;
+
+        while (head != null) {
+            ListNode node = dummy;
+            while (node.next != null && node.next.val < head.val) {
+                node = node.next;
+            }
+            //交换head和head.next
+            ListNode temp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = temp;
+        }
+
+        return dummy.next;
+    }
+}
---
+++ 173_insertion-sort-list/note.md
@@ -0,0 +1,11 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/insertion-sort-list
+@Language: Markdown
+@Datetime: 16-11-10 08:39
+```
+
+dummy的应用
+
+**不懂**
