50705c5c3fc0aed314592b7f5e4c83e19481d67e
Yi Zhan <yizhan42@gmail.com>
2018-11-14 12:07:36 +0800

    Add files via upload

---
+++ 153_combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,51 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/combination-sum-ii
+@Language: Java
+@Datetime: 17-02-25 03:03
+*/
+
+public class Solution {
+    /**
+     * @param num: Given the candidate numbers
+     * @param target: Given the target number
+     * @return: All the combinations that sum to target
+     */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // write your code here
+        List<List<Integer>> results = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) {
+            return results;
+        }
+
+        Arrays.sort(candidates);
+        List<Integer> combination = new ArrayList<Integer>();
+        helper(candidates, 0, combination, target, results);
+
+        return results;
+    }
+
+    private void helper(int[] candidates,
+                        int startIndex,
+                        List<Integer> combination,
+                        int target,
+                        List<List<Integer>> results) {
+        if (target == 0) {
+            results.add(new ArrayList<Integer>(combination));
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.length; i++) {
+            if (i != startIndex && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            if (target < candidates[i]) {
+                break;
+            }
+            combination.add(candidates[i]);
+            helper(candidates, i + 1, combination, target - candidates[i], results);
+            combination.remove(combination.size() - 1);
+        }
+    }
+}
