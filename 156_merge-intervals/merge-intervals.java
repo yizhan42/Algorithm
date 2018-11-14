c28722f284f3367c6bfcb9f114226ced049e3cb4
Yi Zhan <yizhan42@gmail.com>
2018-11-14 14:13:18 +0800

    Add files via upload

---
+++ 156_merge-intervals/merge-intervals.java
@@ -0,0 +1,55 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/merge-intervals
+@Language: Java
+@Datetime: 16-11-10 09:00
+*/
+
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ */
+
+class Solution {
+    /**
+     * @param intervals, a collection of intervals
+     * @return: A new sorted interval list.
+     */
+    public List<Interval> merge(List<Interval> intervals) {
+        // write your code here
+          if (intervals == null || intervals.size() <= 1) {
+            return intervals;
+        }
+        
+        Collections.sort(intervals, new IntervalComparator());       
+  
+        ArrayList<Interval> result = new ArrayList<Interval>();
+        Interval last = intervals.get(0);
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval curt = intervals.get(i);
+            if (curt.start <= last.end ){
+                last.end = Math.max(last.end, curt.end);
+            }else{
+                result.add(last);
+                last = curt;
+            }
+        }
+        
+        result.add(last);
+        return result;
+    }
+    
+    
+    private class IntervalComparator implements Comparator<Interval> {
+        public int compare(Interval a, Interval b) {
+            return a.start - b.start;
+        }
+    }
+
+}
---
+++ 156_merge-intervals/note.md
@@ -0,0 +1,15 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/merge-intervals
+@Language: Markdown
+@Datetime: 16-11-10 09:00
+```
+
+java List 排序 Collections.sort() - 小媛在努力~ - 博客频道 - CSDN.NET  http://blog.csdn.net/zxy_snow/article/details/7232035
+
+用Java集合中的Collections.sort方法对list排序的两种方法 - OPEN 开发经验库  http://www.open-open.com/lib/view/open1322614659562.html
+
+
+1 先把区间按照开头排序。
+2 然后一个一个放进来，如果和前一个重合则合并，否则就直接放进去。
