05d7ff20340cda0517541c3becb318312695f282
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:40:40 +0800

    Add files via upload

---
+++ 225_find-node-in-linked-list/find-node-in-linked-list.java
@@ -0,0 +1,46 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/find-node-in-linked-list
+@Language: Java
+@Datetime: 16-10-10 10:33
+*/
+
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head: the head of linked list.
+     * @param val: an integer
+     * @return: a linked node or null
+     */
+    public ListNode findNode(ListNode head, int val) { 
+        // Write your code here
+        for(ListNode node = head;node != null;node = node.next){
+        if(node.val == val){
+               return node;
+           }
+        }
+          
+           return null;
+        }
+    
+}
+
+/*
+for(ListNode node = head; node != null; node = node.next){
+    if(node.val == val){
+        return node;
+    }
+}
+    return null;
+ */
---
+++ 225_find-node-in-linked-list/note.md
@@ -0,0 +1,10 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/find-node-in-linked-list
+@Language: Markdown
+@Datetime: 16-10-10 10:33
+```
+
+for(ListNode node = head;node != null;node = node.next)
+注意括号里的写法，之前没遇到过。
