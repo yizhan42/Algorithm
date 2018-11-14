6e4c357def8a259878c7750d644aa719cf45b6d1
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:00:39 +0800

    Add files via upload

---
+++ 174_remove-nth-node-from-end-of-list/note.md
@@ -0,0 +1,9 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/remove-nth-node-from-end-of-list
+@Language: Markdown
+@Datetime: 16-10-16 03:15
+```
+
+dummy节点应用
---
+++ 174_remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -0,0 +1,49 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/remove-nth-node-from-end-of-list
+@Language: Java
+@Datetime: 16-10-16 03:15
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer.
+     * @return: The head of linked list.
+     */
+    ListNode removeNthFromEnd(ListNode head, int n) {
+        // write your code here
+          if (n <= 0) {
+            return null;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode preDelete = dummy;
+        for (int i = 0; i < n; i++) {
+            if (head == null) {
+                return null;
+            }
+            head = head.next;
+        }
+        while (head != null) {
+            head = head.next;
+            preDelete = preDelete.next;
+        }
+        preDelete.next = preDelete.next.next;
+        return dummy.next;
+    }
+}
