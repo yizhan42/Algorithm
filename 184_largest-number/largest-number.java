56dd97b1f7887a7804e8fc2cb610d7e54f6126eb
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:04:52 +0800

    Add files via upload

---
+++ 184_largest-number/largest-number.java
@@ -0,0 +1,43 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/largest-number
+@Language: Java
+@Datetime: 16-11-13 13:30
+*/
+
+class NumbersComparator implements Comparator<String> {
+	@Override
+	public int compare(String s1, String s2) {
+		return (s2 + s1).compareTo(s1 + s2);
+	}
+}
+
+public class Solution {
+    /**
+     *@param num: A list of non negative integers
+     *@return: A string
+     */
+    
+    public String largestNumber(int[] nums) {
+        // write your code here
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = Integer.toString(nums[i]);
+        }
+        Arrays.sort(strs, new NumbersComparator());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strs.length; i++) {
+            sb.append(strs[i]);
+        }
+        String result = sb.toString();
+        int index = 0;
+        while (index < result.length() && result.charAt(index) == '0') {
+            index++;
+        }
+        if (index == result.length()) {
+            return "0";
+        }
+        return result.substring(index);
+    }
+}
---
+++ 184_largest-number/note.md
@@ -0,0 +1,12 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/largest-number
+@Language: Markdown
+@Datetime: 16-11-13 13:30
+```
+
+Java数组排序Arrays.sort，以及Comparator接口的用法 - giianhui的专栏 - 博客频道 - CSDN.NET http://blog.csdn.net/giianhui/article/details/5650917
+
+
+JavaScript charAt() 方法  http://www.w3school.com.cn/jsref/jsref_charAt.asp
