b19290fbab9892c2282939e683e5904a5eb97e59
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:03:28 +0800

    Add files via upload

---
+++ 177_convert-sorted-array-to-binary-search-tree-with-minimal-height/convert-sorted-array-to-binary-search-tree-with-minimal-height.java
@@ -0,0 +1,43 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/convert-sorted-array-to-binary-search-tree-with-minimal-height
+@Language: Java
+@Datetime: 16-11-06 01:41
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param A: an integer array
+     * @return: a tree node
+     */
+    private TreeNode buildTree(int[] num ,int start,int end){
+        
+        if (start > end){
+             return null;
+        }
+        
+       TreeNode node = new TreeNode(num[(start + end)/2]); 
+        node.left = buildTree(num,start,(start + end)/2 - 1);
+        node.right = buildTree(num,(start + end)/2 + 1,end);
+        return node;
+     }
+    public TreeNode sortedArrayToBST(int[] A) {  
+        // write your code here
+        if (A == null){
+            return null;
+        }
+        return buildTree(A,0,A.length -1);
+    }  
+}
---
+++ 177_convert-sorted-array-to-binary-search-tree-with-minimal-height/note.md
@@ -0,0 +1,10 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/convert-sorted-array-to-binary-search-tree-with-minimal-height
+@Language: Markdown
+@Datetime: 16-11-06 01:41
+```
+
+第一步编写函数来构建树
+第二步调用构建树的函数
