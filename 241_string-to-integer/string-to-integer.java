72d7143ccd19ff65ac7cb0dbde407b516bfc34e0
Yi Zhan <yizhan42@gmail.com>
2018-11-14 18:40:54 +0800

    Add files via upload

---
+++ 241_string-to-integer/string-to-integer.java
@@ -0,0 +1,18 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/string-to-integer
+@Language: Java
+@Datetime: 16-03-20 12:39
+*/
+
+public class Solution {
+    /**
+     * @param str a string
+     * @return an integer
+     */
+    public int stringToInteger(String str) {
+        // Write your code here
+       return Integer.valueOf(str);
+    }
+}
