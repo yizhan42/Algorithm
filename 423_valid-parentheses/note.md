dd65c878f720f43e8d1db713bc5e9fa5e82e8f09
Yi Zhan <yizhan42@gmail.com>
2018-11-15 13:57:52 +0800

    Add files via upload

---
+++ 423_valid-parentheses/note.md
@@ -0,0 +1,28 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/valid-parentheses
+@Language: Markdown
+@Datetime: 16-11-01 13:48
+```
+
+数据结构-栈和队列 - guisu，程序人生。 逆水行舟，不进则退。 - 博客频道 - CSDN.NET  http://blog.csdn.net/hguisu/article/details/7674195
+
+
+栈的基本运算有六种：
+
+构造空栈：InitStack(S)、
+
+判栈空: StackEmpty(S)、
+
+判栈满： StackFull(S)、
+
+进栈： Push(S,x)、可形象地理解为压入，这时栈中会多一个元素
+
+退栈： Pop(S) 、 可形象地理解为弹出，弹出后栈中就无此元素了。
+
+取栈顶元素：StackTop(S),不同与弹出，只是使用栈顶元素的值，该元素仍在栈顶不会改变。
+
+
+Java.util.Stack类的peek()方法 - wodwl的专栏 - 博客频道 - CSDN.NET  http://blog.csdn.net/wodwl/article/details/4368759
+Java.util.Stack这个类不难，但需要注意其中的peek()方法： 查看栈顶对象而不移除它
---
+++ 423_valid-parentheses/valid-parentheses.java
@@ -0,0 +1,32 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/valid-parentheses
+@Language: Java
+@Datetime: 16-11-01 13:48
+*/
+
+public class Solution {
+    /**
+     * @param s A string
+     * @return whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        // Write your code here
+         Stack<Character> stack = new Stack<Character>();
+         for(int i = 0;i < s.length(); ++i){
+             char ch = s.charAt(i);
+             if('['==ch || '{'==ch || '('==ch){
+                 stack.push(ch);
+             }else if(stack.isEmpty() ){
+                 return false;
+             }else if((']'==ch && stack.peek() !='[')
+             ||('}'==ch &&stack.peek() !='{')
+             ||(')'==ch && stack.peek() !='(')){
+                 return false;
+             }else
+            { stack.pop();}
+         }
+         return stack.isEmpty();
+    }
+}
