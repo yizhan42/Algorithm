78ba86bfb634aed0dd4f65c1023cfbc6edaeedf5
Yi Zhan <yizhan42@gmail.com>
2018-11-15 09:19:59 +0800

    Add files via upload

---
+++ 244_pass-interview/pass-interview.java
@@ -0,0 +1,23 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/pass-interview
+@Language: Java
+@Datetime: 16-03-08 10:51
+*/
+
+public class Solution {
+    /**
+     * @param sex the sex of a candidate
+     * @param score the interview score of a candidate
+     * @return a boolean
+     */
+    public boolean passInterview(boolean sex, double score) {
+        // Write your code here
+        if ((sex == true && score >= 3 ) || (sex == false && score >=4))
+        return true;
+         else
+    return false;
+}
+
+}
