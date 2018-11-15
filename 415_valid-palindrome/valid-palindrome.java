ea2cd0d89a20c9fd01f8248c7206810af1572da6
Yi Zhan <yizhan42@gmail.com>
2018-11-15 13:57:30 +0800

    Add files via upload

---
+++ 415_valid-palindrome/valid-palindrome.java
@@ -0,0 +1,44 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/valid-palindrome
+@Language: Java
+@Datetime: 16-03-29 07:30
+*/
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        int front = 0;
+        int end = s.length() - 1;
+        while (front < end) {
+            while (front < s.length() && !isvalid(s.charAt(front))){ // nead to check range of a/b
+                front++;
+            }
+
+            if (front == s.length()) { // for emtpy string â.,,,â     
+                return true; 
+            }           
+
+            while (end >= 0 && ! isvalid(s.charAt(end))) { // same here, need to check border of a,b
+                end--;
+            }
+
+            if (Character.toLowerCase(s.charAt(front)) != Character.toLowerCase(s.charAt(end))) {
+                break;
+            } else {
+                front++;
+                end--;
+            }
+        }
+
+        return end <= front; 
+    }
+
+    private boolean isvalid (char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
+    }
+}
