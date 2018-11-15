2a5870ee1d01984ed570f3c78efa0363436f39a5
Yi Zhan <yizhan42@gmail.com>
2018-11-15 13:56:08 +0800

    Add files via upload

---
+++ 372_delete-node-in-the-middle-of-singly-linked-list/delete-node-in-the-middle-of-singly-linked-list.java
@@ -0,0 +1,45 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/delete-node-in-the-middle-of-singly-linked-list
+@Language: Java
+@Datetime: 16-10-10 14:15
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param node: the node in the list should be deleted
+     * @return: nothing
+     */
+    public void deleteNode(ListNode node) {
+        // write your code here
+        /*ListNode head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while(head.next != null && head.next.val < node.val){
+            head = head.next;
+        }
+        head = head.next.next;
+        return dummy.next;
+    }
+    */
+    if(node == null || node.next == null)
+        return;
+    ListNode next = node.next;
+    node.val = next.val;
+    node.next = next.next;
+    }
+}
