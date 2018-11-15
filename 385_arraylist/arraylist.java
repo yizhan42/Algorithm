cc8ad8986da0126b637a1d3b941b05ed4a89ffe2
Yi Zhan <yizhan42@gmail.com>
2018-11-15 13:57:07 +0800

    Add files via upload

---
+++ 385_arraylist/arraylist.java
@@ -0,0 +1,77 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/arraylist
+@Language: Java
+@Datetime: 16-10-09 02:20
+*/
+
+public class ArrayListManager {
+    /**
+     * @param n: You should generate an array list of n elements.
+     * @return: The array list your just created.
+     */
+    public static ArrayList<Integer> create(int n) {
+        // Write your code here
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < n; i ++){
+            list.add(i);
+        }
+        return list;
+    }
+    
+    /**
+     * @param list: The list you need to clone
+     * @return: A deep copyed array list from the given list
+     */
+    public static ArrayList<Integer> clone(ArrayList<Integer> list) {
+        // Write your code here
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for(Integer a : list){
+            list1.add(a);
+        }
+        return list1;
+    }
+    
+    /**
+     * @param list: The array list to find the kth element
+     * @param k: Find the kth element
+     * @return: The kth element
+     */
+    public static int get(ArrayList<Integer> list, int k) {
+        // Write your code here
+        return list.get(k);
+    }
+    
+    /**
+     * @param list: The array list
+     * @param k: Find the kth element, set it to val
+     * @param val: Find the kth element, set it to val
+     */
+    public static void set(ArrayList<Integer> list, int k, int val) {
+        // write your code here
+        list.set(k,val);
+    }
+    
+    /**
+     * @param list: The array list to remove the kth element
+     * @param k: Remove the kth element
+     */
+    public static void remove(ArrayList<Integer> list, int k) {
+        // write tour code here
+        list.remove(k);
+    }
+    
+    /**
+     * @param list: The array list.
+     * @param val: Get the index of the first element that equals to val
+     * @return: Return the index of that element
+     */
+    public static int indexOf(ArrayList<Integer> list, int val) {
+        // Write your code here
+        if (list == null){
+            return -1;
+        }
+        return indexOf(list,val);
+    }
+}
---
+++ 385_arraylist/note.md
@@ -0,0 +1,28 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/arraylist
+@Language: Markdown
+@Datetime: 16-10-09 02:20
+```
+
+java int与integer的区别 - 发表是最好的记忆 - 博客园  http://www.cnblogs.com/shenliang123/archive/2011/10/27/2226903.html
+
+int与integer的区别从大的方面来说就是基本数据类型与其包装类的区别：
+
+int 是基本类型，直接存数值，而integer是对象，用一个引用指向这个对象
+
+1.Java 中的数据类型分为基本数据类型和复杂数据类型
+
+int 是前者而integer 是后者（也就是一个类）；因此在类进行初始化时int类的变量初始为0.而Integer的变量则初始化为null.
+
+
+　Java中int和Integer关系是比较微妙的。关系如下：
+
+　　1.int是基本的数据类型；
+
+　　2.Integer是int的封装类；
+
+　　3.int和Integer都可以表示某一个数值；
+
+　　4.int和Integer不能够互用，因为他们两种不同的数据类型；
