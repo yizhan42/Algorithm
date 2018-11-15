042d517ce532ee0947c184a40ad33e4d39b6555b
Yi Zhan <yizhan42@gmail.com>
2018-11-15 09:21:12 +0800

    Add files via upload

---
+++ 283_max-of-3-numbers/max-of-3-numbers.java
@@ -0,0 +1,28 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/max-of-3-numbers
+@Language: Java
+@Datetime: 16-03-09 05:19
+*/
+
+public class Solution {
+    /**
+     * @param num1 an integer
+     * @param num2 an integer
+     * @param num3 an integer
+     * @return an integer
+     */
+    public int maxOfThreeNumbers(int num1, int num2, int num3) {
+        // Write your code here
+        int max = num1;
+        if (num2 >= max){
+            max = num2;
+        }
+        if(num3 >= max){
+            max = num3;
+        }
+    
+    return max;
+}
+}
