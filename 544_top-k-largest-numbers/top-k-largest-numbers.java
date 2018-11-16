f628bbe2d0f132c09d075b38b05014d94dd7884e
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:34:04 +0800

    Add files via upload

---
+++ 544_top-k-largest-numbers/top-k-largest-numbers.java
@@ -0,0 +1,40 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/top-k-largest-numbers
+@Language: Java
+@Datetime: 17-02-25 15:38
+*/
+
+class Solution {
+    /*
+     * @param nums an integer array
+     * @param k an integer
+     * @return the top k largest numbers in array
+     */
+    public int[] topk(int[] nums, int k) {
+        // Write your code here
+         PriorityQueue<Integer> minheap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+             public int compare(Integer o1, Integer o2) {
+                 if(o1 < o2) {
+                     return 1;
+                 } else if(o1 > o2) {
+                     return -1;
+                 } else {
+                     return 0;
+                 }
+             }
+         });
+
+         for (int i : nums) {
+             minheap.add(i);
+         }
+
+         int[] result = new int[k];
+         for (int i = 0; i < result.length; i++) {
+             result[i] = minheap.poll();
+         }
+         return result;
+    }
+};
+
