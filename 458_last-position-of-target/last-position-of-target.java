36fc47adeaa5f5efcb4e7967490c813b7f536679
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:31:29 +0800

    Add files via upload

---
+++ 458_last-position-of-target/last-position-of-target.java
@@ -0,0 +1,53 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/last-position-of-target
+@Language: Java
+@Datetime: 16-12-07 02:15
+*/
+
+public class Solution {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return an integer
+     */
+    public int lastPosition(int[] nums, int target) {
+        // Write your code here
+       /*
+       if(nums == null && nums.length == 0){
+            return -1;
+        }
+        int i = 0;
+        for ( i = 0; i < nums.length ; i++ ){
+            if(target == nums[i]){
+                return i;
+            }
+            else 
+            return -1;
+        } 
+         return i;
+         */
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        int start = 0, end = nums.length - 1;
+        while(start + 1 < end){
+          int mid = (start + end) / 2;
+          if(nums[mid] == target){
+              start = mid;
+          }else if(nums[mid] < target){
+              start = mid;
+          }else{
+              end = mid;
+          }
+        }
+        if(nums[end] == target){
+            return end;
+        }
+        if(nums[start] == target){
+            return start;
+        }
+        return -1;
+    }    
+}
---
+++ 458_last-position-of-target/note.md
@@ -0,0 +1,25 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/last-position-of-target
+@Language: Markdown
+@Datetime: 16-12-07 02:15
+```
+
+1、下标从0开始
+
+2、如何判断数组是否为空？
+http://bbs.csdn.net/topics/10177432
+
+3、java中数组为空和数组长度为0的区别
+http://blog.csdn.net/wangluqinglxq/article/details/38796057
+
+4、int mid = (start + end) / 2;
+int mid = start + (end - start) / 2;
+一样
+
+
+5、当nums[mid] == target时，让start = mid就保证了返回的一定是最后的位置
+
+6、数组nums.length
+     字符串 target.length()
