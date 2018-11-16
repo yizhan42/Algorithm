9ec02f558d016f5bcfe626c8cb489c34548c6e03
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:59:00 +0800

    Add files via upload

---
+++ 491_palindrome-number/palindrome-number.java
@@ -0,0 +1,24 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/palindrome-number
+@Language: Java
+@Datetime: 16-03-09 08:56
+*/
+
+public class Solution {
+    /**
+     * @param num a positive number
+     * @return true if it's a palindrome or false
+     */
+    public boolean palindromeNumber(int num) {
+        // Write your code here
+        int temp = num;
+        int reversed = 0;
+        while (temp != 0) {
+            reversed = reversed * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return reversed == num;
+    }
+}
