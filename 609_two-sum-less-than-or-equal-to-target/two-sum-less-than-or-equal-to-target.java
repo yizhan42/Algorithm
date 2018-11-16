e5d6057976d837283d1619d5bc5cfd52d6e8cef8
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:41:38 +0800

    Add files via upload

---
+++ 609_two-sum-less-than-or-equal-to-target/two-sum-less-than-or-equal-to-target.java
@@ -0,0 +1,35 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/two-sum-less-than-or-equal-to-target
+@Language: Java
+@Datetime: 17-02-25 12:29
+*/
+
+public class Solution {
+    /**
+     * @param nums an array of integer
+     * @param target an integer
+     * @return an integer
+     */
+    public int twoSum5(int[] nums, int target) {
+        // Write your code here
+          if (nums == null || nums.length < 2)
+            return 0;
+
+        Arrays.sort(nums);
+        int cnt = 0;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int v = nums[left] + nums[right];
+            if (v > target) {
+                right --;
+            } else {
+                cnt += right - left;
+                left ++;
+            }
+        }
+        return cnt;
+    
+    }
+}
