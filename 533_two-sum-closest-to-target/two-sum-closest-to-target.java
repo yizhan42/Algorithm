dd31e05711944ee978cbdbf7dde264ae395437f8
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:33:36 +0800

    Add files via upload

---
+++ 533_two-sum-closest-to-target/two-sum-closest-to-target.java
@@ -0,0 +1,38 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/two-sum-closest-to-target
+@Language: Java
+@Datetime: 17-02-25 15:25
+*/
+
+public class Solution {
+    /**
+     * @param nums an integer array
+     * @param target an integer
+     * @return the difference between the sum and the target
+     */
+    public int twoSumClosest(int[] nums, int target) {
+        // Write your code here
+        if (nums == null || nums.length < 2) {
+            return -1;
+        }
+        
+        Arrays.sort(nums);
+        
+        int left = 0, right = nums.length - 1;
+        int diff = Integer.MAX_VALUE;
+        
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                diff = Math.min(diff, target - nums[left] - nums[right]);
+                left++;
+            } else {
+                diff = Math.min(diff, nums[left] + nums[right] - target);
+                right--;
+            }
+        }
+        
+        return diff;
+    }
+}
