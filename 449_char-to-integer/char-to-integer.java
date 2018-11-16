5891c9e35ce75770d1871a4a1c4fcd95ac51c1a1
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:30:13 +0800

    Add files via upload

---
+++ 449_char-to-integer/char-to-integer.java
@@ -0,0 +1,19 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/char-to-integer
+@Language: Java
+@Datetime: 16-03-08 09:45
+*/
+
+public class Solution {
+    /**
+     * @param character a character
+     * @return an integer
+     */
+    public int charToInteger(char character) {
+        // Write your code here
+        
+        return (int) character;
+    }
+}
