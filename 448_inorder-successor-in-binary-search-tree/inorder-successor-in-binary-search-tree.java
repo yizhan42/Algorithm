3e1bf30ec72fa69e4636a6d4c444ed788ae431e3
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:29:58 +0800

    Add files via upload

---
+++ 448_inorder-successor-in-binary-search-tree/inorder-successor-in-binary-search-tree.java
@@ -0,0 +1,46 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/inorder-successor-in-binary-search-tree
+@Language: Java
+@Datetime: 16-12-21 10:50
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        TreeNode successor = null;
+        while (root != null && root.val != p.val) {
+            if (root.val > p.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        
+        if (root == null) {
+            return null;
+        }
+        
+        if (root.right == null) {
+            return successor;
+        }
+        
+        root = root.right;
+        while (root.left != null) {
+            root = root.left;
+        }
+        
+        return root;
+    }
+}
---
+++ 448_inorder-successor-in-binary-search-tree/note.md
@@ -0,0 +1,9 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/inorder-successor-in-binary-search-tree
+@Language: Markdown
+@Datetime: 16-12-21 10:50
+```
+
+注意代码顺序
