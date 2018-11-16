bd3c954128fd34e3b4e89606c674ab22769b3c98
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:39:49 +0800

    Add files via upload

---
+++ 599_insert-into-a-cyclic-sorted-list/insert-into-a-cyclic-sorted-list.java
@@ -0,0 +1,52 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/insert-into-a-cyclic-sorted-list
+@Language: Java
+@Datetime: 17-02-25 12:03
+*/
+
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param node a list node in the list
+     * @param x an integer
+     * @return the inserted new list node
+     */
+    public ListNode insert(ListNode node, int x) {
+        // Write your code here
+        if (node == null) {
+            node = new ListNode(x);
+            node.next = node;
+            return node;
+        }
+
+        ListNode p = node;
+        ListNode prev = null;
+        do {
+            prev = p;
+            p = p.next;
+            if (x <= p.val && x >= prev.val) { 
+                break;
+            }
+            if ((prev.val > p.val) && (x < p.val || x > prev.val)) {
+                break;
+            }
+        } while (p != node);
+
+        ListNode newNode = new ListNode(x);
+        newNode.next = p;
+        prev.next = newNode;
+        return newNode;
+    }
+}
