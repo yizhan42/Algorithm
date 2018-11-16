d68376d177d0a116c2791778622a36185dd90751
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:52:27 +0800

    Add files via upload

---
+++ 480_binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,67 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/binary-tree-paths
+@Language: Java
+@Datetime: 16-11-04 12:10
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of the binary tree
+     * @return all root-to-leaf paths
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        // Write your code here
+       /*
+        List<String> result = new ArrayList<String>();
+        helper(root);
+        return result;
+    }
+    public void helper(TreeNode root){
+        //String str = "root.val";
+        if (root == null){
+            return;
+        }
+        result.add(String.valueOf(root.val));
+        helper(root.left);
+        helper(root.right);
+        */
+        List<String> result = new ArrayList<String>();
+        if (root == null) {
+            return result;
+        }
+        helper(root, String.valueOf(root.val), result);
+        return result;
+    }
+    
+    private void helper(TreeNode root, String path, List<String> result) {
+        if (root == null) {
+            return;
+        }
+        
+        if (root.left == null && root.right == null) {
+            result.add(path);
+            return;
+        }
+        
+        if (root.left != null) {
+            helper(root.left, path + "->" + String.valueOf(root.left.val), result);
+        }
+        
+        if (root.right != null) {
+            helper(root.right, path + "->" + String.valueOf(root.right.val), result);
+        }
+    }
+}
---
+++ 480_binary-tree-paths/note.md
@@ -0,0 +1,17 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/binary-tree-paths
+@Language: Markdown
+@Datetime: 16-11-04 12:10
+```
+
+**path理解不好**
+
+String、String.valueOf、toString 它们三者的区别总结 - Mr唐 - 博客园  http://www.cnblogs.com/tyhJava/p/5693238.html
+
+当把最后两个if中的helper变成helpler(root.left,path +"->" + root.left.val, result); 时，也能AC。
+
+1->2->5 中 （1->）是一组，（2->）是一组，剩下一个5，就如同只有根节点
+
+
