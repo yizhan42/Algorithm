0855f28260327f95f2eb1ceb55f3cc6ebb8b8d33
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:30:26 +0800

    Add files via upload

---
+++ 452_remove-linked-list-elements/note.md
@@ -0,0 +1,9 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/remove-linked-list-elements
+@Language: Markdown
+@Datetime: 16-03-30 14:09
+```
+
+dummy节点应用
---
+++ 452_remove-linked-list-elements/remove-linked-list-elements.java
@@ -0,0 +1,39 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/remove-linked-list-elements
+@Language: Java
+@Datetime: 16-03-30 14:09
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head a ListNode
+     * @param val an integer
+     * @return a ListNode
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        // Write your code here
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        
+        return dummy.next;
+    }
+}
