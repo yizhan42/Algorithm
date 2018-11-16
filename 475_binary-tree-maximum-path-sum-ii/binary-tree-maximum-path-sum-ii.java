1d964fad4e2bb6cfca0abd7b34c3b0465accd2fa
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:32:28 +0800

    Add files via upload

---
+++ 475_binary-tree-maximum-path-sum-ii/binary-tree-maximum-path-sum-ii.java
@@ -0,0 +1,35 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/binary-tree-maximum-path-sum-ii
+@Language: Java
+@Datetime: 16-12-20 12:28
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of binary tree.
+     * @return an integer
+     */
+    public int maxPathSum2(TreeNode root) {
+        // Write your code here
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        
+        int left = maxPathSum2(root.left);
+        int right = maxPathSum2(root.right);
+        return root.val + Math.max(0, Math.max(left, right));
+    }
+}
