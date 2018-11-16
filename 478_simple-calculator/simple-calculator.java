6d1f5adc34490676dd48d1af9db8bd6356eaa052
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:32:46 +0800

    Add files via upload

---
+++ 478_simple-calculator/simple-calculator.java
@@ -0,0 +1,29 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/simple-calculator
+@Language: Java
+@Datetime: 16-09-25 01:42
+*/
+
+public class Calculator {
+    /**
+      * @param a, b: Two integers.
+      * @param operator: A character, +, -, *, /.
+      */
+    public int calculate(int a, char operator, int b) {
+        /* your code */
+        
+        switch(operator){
+            case '+':System.out.println(a+b);
+            break;
+            case '-':System.out.println(a-b);
+            break;
+            case '*':System.out.println(a*b);
+            break;
+            case '/':System.out.println(a/b);
+            break;
+        }
+    return 0;
+    }
+}
