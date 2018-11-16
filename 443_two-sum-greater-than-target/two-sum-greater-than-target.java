f6648c9553bc7fcacd51bd57cb432ac78a24473a
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:29:26 +0800

    Add files via upload

---
+++ 443_two-sum-greater-than-target/two-sum-greater-than-target.java
@@ -0,0 +1,36 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/two-sum-greater-than-target
+@Language: Java
+@Datetime: 17-02-25 12:20
+*/
+
+public class Solution {
+    /**
+     * @param nums: an array of integer
+     * @param target: an integer
+     * @return: an integer
+     */
+    public int twoSum2(int[] nums, int target) {
+        // Write your code here
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+        
+        int left = 0, right = nums.length - 1;
+        int count = 0;
+        while (left < right) {
+            if (nums[left] + nums[right] <= target) {
+                left++;
+            } else {
+                count += right - left;
+                right--;
+            }
+        }
+        
+        return count;
+    }
+}
