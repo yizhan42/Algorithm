a431c2cb6670f6c65ad9642efe48886ee4573eeb
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:40:02 +0800

    Add files via upload

---
+++ 602_russian-doll-envelopes/russian-doll-envelopes.java
@@ -0,0 +1,39 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/russian-doll-envelopes
+@Language: Java
+@Datetime: 17-02-25 16:01
+*/
+
+public class Solution {
+    /**
+     * @param envelopes a number of envelopes with widths and heights
+     * @return the maximum number of envelopes
+     */
+    public int maxEnvelopes(int[][] envelopes) {
+        // Write your code here
+        if(envelopes == null || envelopes.length == 0 
+            || envelopes[0] == null || envelopes[0].length != 2)
+            return 0;
+        Arrays.sort(envelopes, new Comparator<int[]>(){
+            public int compare(int[] arr1, int[] arr2){
+                if(arr1[0] == arr2[0])
+                    return arr2[1] - arr1[1];
+                else
+                    return arr1[0] - arr2[0];
+            } 
+        });
+        int dp[] = new int[envelopes.length];
+        int len = 0;
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+                if(index < 0)
+                    index = -index - 1;
+            dp[index] = envelope[1];
+            if (index == len)
+                len++;
+        }
+        return len;
+    }
+}
