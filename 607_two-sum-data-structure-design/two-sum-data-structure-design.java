7aa7725943793ded1b82aec49e4b9251822ba268
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:41:15 +0800

    Add files via upload

---
+++ 607_two-sum-data-structure-design/two-sum-data-structure-design.java
@@ -0,0 +1,47 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/two-sum-data-structure-design
+@Language: Java
+@Datetime: 17-02-25 12:28
+*/
+
+public class TwoSum {
+
+
+    private List<Integer> list = null;
+    private Map<Integer, Integer> map = null;
+    public TwoSum() {
+        list = new ArrayList<Integer>();
+        map = new HashMap<Integer, Integer>();
+    }
+    
+    // Add the number to an internal data structure.
+    public void add(int number) {
+        // Write your code here
+        if (map.containsKey(number)) {
+            map.put(number, map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+            list.add(number);
+        }
+    }
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+    public boolean find(int value) {
+        // Write your code here
+        for (int i = 0; i < list.size(); i++) {
+            int num1 = list.get(i), num2 = value - num1;
+            if ((num1 == num2 && map.get(num1) > 1) || 
+                (num1 != num2 && map.containsKey(num2))) 
+                return true;
+        }
+        return false;
+    }
+}
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
