0aaec6166406b802772a92cd4c902d99c05ad985
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:52:14 +0800

    Add files via upload

---
+++ 479_second-max-of-array/second-max-of-array.java
@@ -0,0 +1,30 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/second-max-of-array
+@Language: Java
+@Datetime: 16-03-07 12:44
+*/
+
+import java.lang.*;
+ public class Solution {
+    /**
+     * @param nums: An integer array.
+     * @return: The second max number in the array.
+     */
+    public int secondMax(int[] nums) {
+        int max= Math.max(nums[0], nums[1]);
+        int second = Math.min(nums[0], nums[1]);
+       for (int i = 2; i < nums.length; i++){
+            if (nums[i] >= max){
+                second = max;
+                max = nums[i];
+            } else if (nums[i] > second && nums[i] < max){
+                second = nums[i]; }
+            else {
+                second = Math.min(nums[0], nums[1]); 
+                }
+        }
+        return second;
+    }
+}
