b8ac1633564f3d0404bc2199449d96bd10008c0c
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:57:18 +0800

    Add files via upload

---
+++ 485_generate-arraylist-with-given-size/generate-arraylist-with-given-size.java
@@ -0,0 +1,30 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/generate-arraylist-with-given-size
+@Language: Java
+@Datetime: 16-11-04 11:33
+*/
+
+public class Solution {
+    /**
+     * @param size an integer
+     * @return an array list
+     */
+    public ArrayList<Integer> generate(int size) {
+        // Write your code here 
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i = 1 ; i <= size ; i ++){
+            result.add(i);
+        }
+        return result;
+    }
+}
+
+/*
+for (int i = 1; i <= size; i++ ){
+        result.add(i);
+    }
+    return result;
+}
+*/
---
+++ 485_generate-arraylist-with-given-size/note.md
@@ -0,0 +1,9 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/generate-arraylist-with-given-size
+@Language: Markdown
+@Datetime: 16-11-04 11:33
+```
+
+add()是动态数组ArrayList的函数
