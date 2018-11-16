b0f30c73bf7a5685f940150115e2745f1214d00f
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:32:53 +0800

    Add files via upload

---
+++ 513_perfect-squares/perfect-squares.java
@@ -0,0 +1,33 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/perfect-squares
+@Language: Java
+@Datetime: 17-02-25 15:55
+*/
+
+// version 0 DP
+
+public class Solution {
+    /**
+     * @param n a positive integer
+     * @return an integer
+     */
+    public int numSquares(int n) {
+        // Write your code here
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for(int i = 0; i * i <= n; ++i) {
+            dp[i * i] = 1;
+        }
+
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    
+    }
+}
