e36b9597ba40949b21ba68be0dc8ce01191f052e
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:34:22 +0800

    Add files via upload

---
+++ 546_implement-queue-by-interface/implement-queue-by-interface.java
@@ -0,0 +1,82 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/implement-queue-by-interface
+@Language: Java
+@Datetime: 16-10-31 00:50
+*/
+
+class Node {
+    public int val;
+    public Node next, prev;
+    public Node(int _val) {
+        val = _val;
+        prev = next = null;
+    }
+}
+
+interface InterfaceQueue {
+    void push(int val);
+
+    // define an interface for pop method
+    /* write your code here */
+    int pop();
+ // define an interface for top method
+    /* write your code here */
+    int top();
+}
+
+public class MyQueue implements InterfaceQueue {
+    /* you can declare your private attributes here */
+    public Node first,last;
+    public MyQueue() {
+       // do initialization if necessary
+    }
+
+    // implement the push method
+    /* write your code here */
+ public void push(int val) {
+        if (last == null) {
+            last = new Node(val);
+            first = last;		
+        } else {
+            Node node = new Node(val);
+            last.next = node;
+            node.prev = last;
+            last = last.next;
+        }
+    }
+
+    @Override
+    public int pop() {
+        /* write your code here */
+        if (first != null) {
+            int val = first.val;
+            first = first.next;
+            if (first != null)
+                first.prev = null;
+            else
+                last = null;
+            return val;
+        }
+        return -1;
+    }
+    
+
+    @Override
+    public int top() {
+        /* write your code here */
+        if (first != null) {
+            return first.val;
+        }
+        return -1;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue queue = new MyQueue();
+ * queue.push(123);
+ * queue.top(); will return 123;
+ * queue.pop(); will return 123 and pop the first element in the queue
+ */
---
+++ 546_implement-queue-by-interface/note.md
@@ -0,0 +1,21 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/implement-queue-by-interface
+@Language: Markdown
+@Datetime: 16-10-31 00:50
+```
+
+1、接口里的“抽象方法”只要做声明即可，而不用定义其处理的方式。
+2、数据成员 必须赋初值
+3、利用接口打造新的类的过程，称之为接口的实现。以下为接口实现的语法。
+class 类名称 implements 接口A,接口B
+{
+   ...
+}
+4、一个类只能继承一个父类，但却可以实现多个接口。
+5、一个接口可以继承多个接口
+
+**pop()弹出第一个元素
+top()读取第一个元素
+stack.peek()读取第一个元素而不弹出**
