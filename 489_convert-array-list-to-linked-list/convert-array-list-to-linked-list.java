6e5a0c2f563c1047cf49f81937fa2563983634ca
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:58:24 +0800

    Add files via upload

---
+++ 489_convert-array-list-to-linked-list/convert-array-list-to-linked-list.java
@@ -0,0 +1,42 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/convert-array-list-to-linked-list
+@Language: Java
+@Datetime: 16-10-11 08:54
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param nums an integer array list
+     * @return the first node of linked list
+     */
+    public ListNode toLinkedList(ArrayList<Integer> nums) {  
+        // Write your code here
+        if(nums.size() == 0){
+            return null;
+        }
+        ListNode head = null;
+        ListNode p = null;
+        for(Integer num:nums){
+        if(head == null){
+            head = new ListNode(num);
+            p = head;
+         }else{
+         p.next = new ListNode(num);
+         p = p.next;}
+        }
+        return head;
+    }
+}
---
+++ 489_convert-array-list-to-linked-list/note.md
@@ -0,0 +1,11 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/convert-array-list-to-linked-list
+@Language: Markdown
+@Datetime: 16-10-11 08:54
+```
+
+1、JAVA 中ArrayList的用法!_360问答  http://wenda.so.com/q/1379452690067833?src=140
+
+2、Java中ArrayList类的用法(转) - 吴勇寿 - 博客园  http://www.cnblogs.com/YSO1983/archive/2011/08/22/2149711.html
