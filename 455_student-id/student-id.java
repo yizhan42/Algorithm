585b94d646322a2581854486663050f00efcbdde
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:31:12 +0800

    Add files via upload

---
+++ 455_student-id/student-id.java
@@ -0,0 +1,37 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/student-id
+@Language: Java
+@Datetime: 16-03-20 04:20
+*/
+
+class Student {
+    public int id;
+    
+    public Student(int id) {
+        this.id = id;
+    }
+}
+
+public class Class {
+    /**
+     * Declare a public attribute `students` which is an array of `Student`
+     * instances
+     */
+    // write your code here.
+    
+     public Student[] students;
+    /**
+     * Declare a constructor with a parameter n which is the total number of
+     * students in the *class*. The constructor should create n Student
+     * instances and initialized with student id from 0 ~ n-1
+     */
+    // write your code here
+    public Class(int n){
+        students = new Student[n];
+    for (int i = 0; i <= n-1; i++){
+        students[i] = new Student(i);
+    }
+    }
+}
