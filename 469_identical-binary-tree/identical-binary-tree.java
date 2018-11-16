4d8b16bf5b1ea17999b12d7c945a19d9e133ddee
Yi Zhan <yizhan42@gmail.com>
2018-11-16 10:32:14 +0800

    Add files via upload

---
+++ 469_identical-binary-tree/identical-binary-tree.java
@@ -0,0 +1,35 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/identical-binary-tree
+@Language: Java
+@Datetime: 16-11-05 11:26
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param a, b, the root of binary trees.
+     * @return true if they are identical, or false.
+     */
+    public boolean isIdentical(TreeNode a, TreeNode b) {
+        // Write your code here
+        if ((a == null) && (b == null)){
+            return true;
+        }
+        if (a != null && b != null){
+            return (a.val == b.val) && isIdentical(a.left,b.left) && isIdentical(a.right,b.right);
+        }
+        return false;
+    }
+}
