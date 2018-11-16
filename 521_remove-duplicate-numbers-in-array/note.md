0d0d234ff75a6c45435e754b7688f80f1493fb6f
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:33:05 +0800

    Add files via upload

---
+++ 521_remove-duplicate-numbers-in-array/note.md
@@ -0,0 +1,12 @@
+```
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/remove-duplicate-numbers-in-array
+@Language: Markdown
+@Datetime: 16-09-25 03:52
+```
+
+HashMap应用
+
+
+介绍map.entry接口 - lintong - 博客园  http://www.cnblogs.com/lintong/p/4365743.html
---
+++ 521_remove-duplicate-numbers-in-array/remove-duplicate-numbers-in-array.java
@@ -0,0 +1,26 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/remove-duplicate-numbers-in-array
+@Language: Java
+@Datetime: 16-09-25 03:52
+*/
+
+public class Solution {
+    /**
+     * @param nums an array of integers
+     * @return the number of unique integers
+     */
+    public int deduplication(int[] nums) {
+        // Write your code here
+        HashMap<Integer, Boolean> mp = new HashMap<Integer, Boolean>();
+        for (int i = 0; i < nums.length; ++i)
+            mp.put(nums[i], true);
+
+        int result = 0;
+        for (Map.Entry<Integer, Boolean> entry : mp.entrySet())
+            nums[result++] = entry.getKey();
+        return result;
+    }
+}
+
