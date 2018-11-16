4b425cea0db64c829c493d0b47063261b3ccd361
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:33:49 +0800

    Add files via upload

---
+++ 539_move-zeroes/move-zeroes.cpp
@@ -0,0 +1,25 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/move-zeroes
+@Language: C++
+@Datetime: 17-02-25 12:17
+*/
+
+class Solution {
+public:
+    /**
+     * @param nums an integer array
+     * @return nothing, do this in-place
+     */
+    void moveZeroes(vector<int>& nums) {
+        // Write your code here
+         int left = 0, right = 0;
+        while (right < nums.size()) {
+            if (nums[right]) {
+                swap(nums[left++], nums[right]);
+            }
+            right++;
+        }
+    }
+};
