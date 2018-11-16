5f42e091e75175ef9bb0ffcdddb360b740691b99
Yi Zhan <yizhan42@gmail.com>
2018-11-16 11:38:52 +0800

    Add files via upload

---
+++ 596_minimum-subtree/minimum-subtree.java
@@ -0,0 +1,46 @@
+/*
+@Copyright:LintCode
+@Author:   zhanyi
+@Problem:  http://www.lintcode.com/problem/minimum-subtree
+@Language: Java
+@Datetime: 17-02-24 12:46
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of binary tree
+     * @return the root of the minimum subtree
+     */
+     // version 1 : traverse + divide conquer
+     private TreeNode subtree = null;
+    private int subtreeSum = Integer.MAX_VALUE;
+    public TreeNode findSubtree(TreeNode root) {
+        // Write your code here
+         helper(root);
+        return subtree;
+    }
+    
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int sum = helper(root.left) + helper(root.right) + root.val;
+        if (sum < subtreeSum) {
+            subtreeSum = sum;
+            subtree = root;
+        }
+        return sum;
+    }
+}
